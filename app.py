0d31f8b61a74723b57b7866d0319efdee0b66e50
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2024-11-07 10:44:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+from flask import Flask
+from controllers import users, books, emprestimos
+
+app = Flask(__name__)
+app.register_blueprint(users.bp)
+app.register_blueprint(books.bp)
+app.register_blueprint(emprestimos.bp)
 
